35cb9714e8c6aad70483a6890f96ab22359f391e
Максим <rfqa14072011@rambler.ru>
2017-04-17 00:59:05 +0400

    Казанцев М.А Вариант 13 Задача 8

---
+++ ist/KAZANTCEV_M_A/Task8.py
@@ -0,0 +1,38 @@
+# Казанцев Максим
+# Задача 9. Вариант 13.
+# 17.03.2017
+#Доработайте игру "Анаграммы" (см. М.Доусон Программируем на Python. Гл.4) так, чтобы к каждому слову полагалась подсказка. Игрок должен получать право на подсказку в том случае, если у него нет никаких предположений. Разработайте систему начисления очков, по которой бы игроки, отгадавшие слово без подсказки, получали больше тех, кто запросил подсказку.
+
+import random
+
+score = 10
+words = ("want", "pass", "exam", "please")
+word = random.choice(words)
+l = len(word)
+print("В загаданном слове", l, " букв.")
+anagram = list(word)
+random.shuffle(anagram)
+
+i = 0
+print(anagram)
+answer = ""
+while (answer != word):
+    print("Вы назовете слово?(y/n)")
+    answer = str(input())
+    if (answer == str("y")):
+        print("Ответ: ")
+        answer = str(input())
+        if (answer == word):
+            if (score < 0):
+                score == 0
+            print("Вы угадали. Ваш счет: ", str(score))
+    else:
+        print("Хотите взять подсказку(y/n)")
+        answer = str(input())
+        if (answer == str("y")):
+            print("Вы использовали подсказку, но у вас сгорело два очка.", i + 1, "буква: ", word[i])
+            score -= 2
+        else:
+            print("До свидания")
+            break
+input("\nНажмите Enter для выхода.")
