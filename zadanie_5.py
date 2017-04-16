3569721c338efe40d57eab2b38d91654f70445ab
PavelPyatkin <x0l0d42@yandex.ru>
2017-04-16 18:40:21 +0300

    Delete zadanie_5.py

--- zadanie_5.py
+++
@@ -1,7 +0,0 @@
-# Pyatkin Pavel Yur`evich
-# Variant 11
-
-import random
-TheNamesOfSantaClausDeer=['Дэшер', 'Дэнсер', 'Прэнсер', 'Виксен', 'Комет', 'Кьюпид', 'Дондер', 'Блитцен', 'Рудольф']
-print(random.choice(TheNamesOfSantaClausDeer))
-input('Нажмите Enter')
