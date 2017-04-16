aee29043d1eb5d9722d7639540ff628bb920eee9
PavelPyatkin <x0l0d42@yandex.ru>
2017-04-16 18:40:01 +0300

    Delete zadanie_2.py

--- zadanie_2.py
+++
@@ -1,6 +0,0 @@
-# Pyatkin Pavel Yur`evich
-# Variant 11
-
-print ("Тот не даст себя в обиду в этом мире, кто умеет разбираться в женских фокусах и увертках.")
-print ("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tО`Генри")
-input("\n\nНажмите Enter, чтобы выйти.")
